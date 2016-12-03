3aa16eb878883330081ce819391fdb5ac782a657
Wesley W. Terpstra <wesley@sifive.com>
2016-12-02 17:15:15 -0800

    uart: support SMP

--- machine/mtrap.c
+++ machine/mtrap.c
@@ -92,9 +92,19 @@ uintptr_t timer_interrupt()
 static uintptr_t mcall_console_putchar(uint8_t ch)
 {
   if (uart) {
+#ifdef __riscv_atomic
+    int32_t r;
+    do {
+      __asm__ __volatile__ (
+        "amoor.w %0, %2, %1\n"
+        : "=r" (r), "+A" (uart[UART_REG_TXFIFO])
+        : "r" (ch));
+    } while (r < 0);
+#else
     volatile uint32_t *tx = uart + UART_REG_TXFIFO;
     while ((int32_t)(*tx) < 0); // TODO: Use amoswap.w
     *tx = ch;
+#endif
   } else {
     do_tohost_fromhost(1, 1, ch);
   }
