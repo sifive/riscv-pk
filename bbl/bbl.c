ecac7186cadbb6169730a358c628750126880713
Paul Walmsley <paul.walmsley@sifive.com>
2019-03-05 22:52:29 -0800

    bbl: relocate DTB to a higher address
    The default relocation address for the DTB is too low in memory;
    the kernel can overwrite it during memory allocations before the
    DTB is fully parsed.  Fix by placing the DTB higher in memory,
    and report the relocated address during boot.

--- bbl/bbl.c
+++ bbl/bbl.c
@@ -24,7 +24,7 @@ static uintptr_t dtb_output()
    * thus mandating device-tree is in physical memory after the kernel.
    */
   uintptr_t end = kernel_end ? (uintptr_t)kernel_end : (uintptr_t)&_payload_end;
-  return (end + MEGAPAGE_SIZE - 1) / MEGAPAGE_SIZE * MEGAPAGE_SIZE;
+  return (((end + MEGAPAGE_SIZE - 1) / MEGAPAGE_SIZE) + 16) * MEGAPAGE_SIZE;
 }
 
 static void filter_dtb(uintptr_t source)
@@ -32,6 +32,7 @@ static void filter_dtb(uintptr_t source)
   uintptr_t dest = dtb_output();
   uint32_t size = fdt_size(source);
   memcpy((void*)dest, (void*)source, size);
+  printm("Relocated DTB from %lx to %lx\n", source, dest);
 
   // Remove information from the chained FDT
   filter_harts(dest, &disabled_hart_mask);
