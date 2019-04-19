fcdde5b891113c367f97ba7f2fbebbdbb139f7ae
Paul Walmsley <paul.walmsley@sifive.com>
2019-04-19 14:55:31 -0700

    machine: uart: look for the compatible string used by Linux
    The BBL code only looks at the first entry in an array of compatible
    strings.  This causes problems with Linux-oriented DT files, since the
    correct initial DT compatible entry will be "sifive,fu540-c000-uart"
    for the FU540 UART.  Take the easy way out and look for this instead
    of "sifive,uart0".

--- machine/uart.c
+++ machine/uart.c
@@ -45,7 +45,7 @@ static void uart_open(const struct fdt_scan_node *node, void *extra)
 static void uart_prop(const struct fdt_scan_prop *prop, void *extra)
 {
   struct uart_scan *scan = (struct uart_scan *)extra;
-  if (!strcmp(prop->name, "compatible") && !strcmp((const char*)prop->value, "sifive,uart0")) {
+  if (!strcmp(prop->name, "compatible") && !strcmp((const char*)prop->value, "sifive,fu540-c000-uart")) {
     scan->compat = 1;
   } else if (!strcmp(prop->name, "reg")) {
     fdt_get_address(prop->node->parent, prop->value, &scan->reg);
